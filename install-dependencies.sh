e2e8226b38977dae4aa55ced989851fcf381fc65
Richard Burhans <burhans@bx.psu.edu>
2017-11-29 12:15:16 -0500

    initial attempt at using Travis CI

---
+++ install-dependencies.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+set -o nounset
+set -o errexit
+
+## update list of available packages
+apt-get -qq update || {
+    echo "failed updating list of available packages"
+    exit 1
+}
+
+## enable snap packages: https://snapcraft.io/
+apt-get install -y snapd || {
+    echo "failed enabling snap packages"
+    exit 1
+}
+
+## install hugo: https://gohugo.io/
+snap install hugo || {
+    echo "failed installing hugo"
+    exit 1
+}
+
+## install HTMLProofer
+NOKOGIRI_USE_SYSTEM_LIBRARIES=true gem install --source https://rubygems.org html-proofer || {
+    echo "failed installing HTMLProofer"
+    exit 1
+}
+
+exit 0
