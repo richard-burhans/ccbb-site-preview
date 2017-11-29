a12d439b456a364dc817ca22939489864e39bd18
Richard Burhans <burhans@bx.psu.edu>
2017-11-29 13:59:05 -0500

    set up rvm environment by hand

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,51 +4,30 @@ set -o nounset
 set -o errexit
 
 ## update list of available packages
+echo "INFO: updating list of available packages"
 apt-get -qq update || {
-    echo "failed updating list of available packages"
+    echo "ERROR: failed updating list of available packages"
     exit 1
 }
 
 ## enable snap packages: https://snapcraft.io/
+echo "INFO: enabling snap packages"
 apt-get install -y snapd || {
-    echo "failed enabling snap packages"
+    echo "ERROR: failed enabling snap packages"
     exit 1
 }
 
 ## install hugo: https://gohugo.io/
+echo "INFO: installing hugo"
 snap install hugo || {
-    echo "failed installing hugo"
+    echo "ERROR: failed installing hugo"
     exit 1
 }
 
-# /home/travis/.rvmrc
-# rvm_autoupdate_flag='0'
-# rvm_binary_flag='1'
-# rvm_fuzzy_flag='1'
-# rvm_gem_options='--no-ri --no-rdoc'
-# rvm_max_time_flag='5'
-# rvm_path='/home/travis/.rvm'
-# rvm_project_rvmrc='0'
-# rvm_remote_server_type4='rubies'
-# rvm_remote_server_url4='https://s3.amazonaws.com/travis-rubies/binaries'
-# rvm_remote_server_verify_downloads4='1'
-# rvm_silence_path_mismatch_check_flag='1'
-# rvm_user_install_flag='1'
-# rvm_with_default_gems="rake bundler"
-# rvm_without_gems="rubygems-bundler"
-
 ## set up rvm environment
-#. $HOME/.rvmrc
-
-#rvm_path='/home/travis/.rvm'
-
-#(rvm_path='/home/travis/.rvm'; echo "$rvm_path/scripts/rvm"; cat "$rvm_path/scripts/rvm") || {
-#    echo "$rvm_path/scripts/rvm DOES NOT EXIST"
-#}
-
-## add rvm bin directory to path
-#PATH=${PATH:+${PATH}:}$HOME/.rvm/bin
-#export PATH
+source "$HOME/.rvmrc"
+source "$rvm_path/scripts/rvm"
+export PATH="${PATH:+${PATH}:}$rvm_bin_path"
 
 ## use rvm default ruby
 rvm use default || {
