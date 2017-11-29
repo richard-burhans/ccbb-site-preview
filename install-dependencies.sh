d4cc3845fc7a2ef196c801ad4a70b63cd390d4a7
Richard Burhans <burhans@bx.psu.edu>
2017-11-29 13:51:25 -0500

    switch to bash

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -o nounset
 set -o errexit
@@ -21,18 +21,34 @@ snap install hugo || {
     exit 1
 }
 
+# /home/travis/.rvmrc
+# rvm_autoupdate_flag='0'
+# rvm_binary_flag='1'
+# rvm_fuzzy_flag='1'
+# rvm_gem_options='--no-ri --no-rdoc'
+# rvm_max_time_flag='5'
+# rvm_path='/home/travis/.rvm'
+# rvm_project_rvmrc='0'
+# rvm_remote_server_type4='rubies'
+# rvm_remote_server_url4='https://s3.amazonaws.com/travis-rubies/binaries'
+# rvm_remote_server_verify_downloads4='1'
+# rvm_silence_path_mismatch_check_flag='1'
+# rvm_user_install_flag='1'
+# rvm_with_default_gems="rake bundler"
+# rvm_without_gems="rubygems-bundler"
+
 ## set up rvm environment
 #. $HOME/.rvmrc
 
 #rvm_path='/home/travis/.rvm'
 
-(rvm_path='/home/travis/.rvm'; echo "$rvm_path/scripts/rvm"; cat "$rvm_path/scripts/rvm") || {
-    echo "$rvm_path/scripts/rvm DOES NOT EXIST"
-}
+#(rvm_path='/home/travis/.rvm'; echo "$rvm_path/scripts/rvm"; cat "$rvm_path/scripts/rvm") || {
+#    echo "$rvm_path/scripts/rvm DOES NOT EXIST"
+#}
 
 ## add rvm bin directory to path
-PATH=${PATH:+${PATH}:}$HOME/.rvm/bin
-export PATH
+#PATH=${PATH:+${PATH}:}$HOME/.rvm/bin
+#export PATH
 
 ## use rvm default ruby
 rvm use default || {
