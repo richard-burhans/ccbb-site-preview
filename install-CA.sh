06a69f8365352761153aec9e1483486a4e1d23e2
Richard Burhans <burhans@bx.psu.edu>
2017-11-29 16:34:29 -0500

    add USERTrust RSA Certification Authority

--- install-CA.sh
+++ install-CA.sh
@@ -1,50 +1,12 @@
 #!/usr/bin/env bash
 
-echo 51e14b4c734e450402ea2cf73f2aee0f
-md5sum /usr/share/ca-certificates/mozilla/AddTrust_External_Root.crt
-
-find /usr/share/ca-certificates -type f -name \*.crt | xargs md5sum
-
-ls -al /etc/ssl/certs
+# 51e14b4c734e450402ea2cf73f2aee0f  /usr/share/ca-certificates/mozilla/AddTrust_External_Root.crt
+# 890d62be87a44342020daec2f09e4a83  /usr/share/ca-certificates/ccbb/USERTrustRSAAddTrustCA.crt
 
 CCBB_CERT_ROOT_DIR=/usr/share/ca-certificates/ccbb
-
 mkdir -p $CCBB_CERT_ROOT_DIR
 
-cat > $CCBB_CERT_ROOT_DIR/addtrustexternalcaroot.crt <<EndOfCert
------BEGIN CERTIFICATE-----
-MIIENjCCAx6gAwIBAgIBATANBgkqhkiG9w0BAQUFADBvMQswCQYDVQQGEwJTRTEU
-MBIGA1UEChMLQWRkVHJ1c3QgQUIxJjAkBgNVBAsTHUFkZFRydXN0IEV4dGVybmFs
-IFRUUCBOZXR3b3JrMSIwIAYDVQQDExlBZGRUcnVzdCBFeHRlcm5hbCBDQSBSb290
-MB4XDTAwMDUzMDEwNDgzOFoXDTIwMDUzMDEwNDgzOFowbzELMAkGA1UEBhMCU0Ux
-FDASBgNVBAoTC0FkZFRydXN0IEFCMSYwJAYDVQQLEx1BZGRUcnVzdCBFeHRlcm5h
-bCBUVFAgTmV0d29yazEiMCAGA1UEAxMZQWRkVHJ1c3QgRXh0ZXJuYWwgQ0EgUm9v
-dDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALf3GjPm8gAELTngTlvt
-H7xsD821+iO2zt6bETOXpClMfZOfvUq8k+0DGuOPz+VtUFrWlymUWoCwSXrbLpX9
-uMq/NzgtHj6RQa1wVsfwTz/oMp50ysiQVOnGXw94nZpAPA6sYapeFI+eh6FqUNzX
-mk6vBbOmcZSccbNQYArHE504B4YCqOmoaSYYkKtMsE8jqzpPhNjfzp/haW+710LX
-a0Tkx63ubUFfclpxCDezeWWkWaCUN/cALw3CknLa0Dhy2xSoRcRdKn23tNbE7qzN
-E0S3ySvdQwAl+mG5aWpYIxG3pzOPVnVZ9c0p10a3CitlttNCbxWyuHv77+ldU9U0
-WicCAwEAAaOB3DCB2TAdBgNVHQ4EFgQUrb2YejS0Jvf6xCZU7wO94CTLVBowCwYD
-VR0PBAQDAgEGMA8GA1UdEwEB/wQFMAMBAf8wgZkGA1UdIwSBkTCBjoAUrb2YejS0
-Jvf6xCZU7wO94CTLVBqhc6RxMG8xCzAJBgNVBAYTAlNFMRQwEgYDVQQKEwtBZGRU
-cnVzdCBBQjEmMCQGA1UECxMdQWRkVHJ1c3QgRXh0ZXJuYWwgVFRQIE5ldHdvcmsx
-IjAgBgNVBAMTGUFkZFRydXN0IEV4dGVybmFsIENBIFJvb3SCAQEwDQYJKoZIhvcN
-AQEFBQADggEBALCb4IUlwtYj4g+WBpKdQZic2YR5gdkeWxQHIzZlj7DYd7usQWxH
-YINRsPkyPef89iYTx4AWpb9a/IfPeHmJIZriTAcKhjW88t5RxNKWt9x+Tu5w/Rw5
-6wwCURQtjr0W4MHfRnXnJK3s9EK0hZNwEGe6nQY1ShjTK3rMUUKhemPR5ruhxSvC
-Nr4TDea9Y355e6cJDUCrat2PisP29owaQgVR1EX1n6diIWgVIEM8med8vSTYqZEX
-c4g/VhsxOBi0cQ+azcgOno4uG+GMmIPLHzHxREzGBHNJdmAPx/i9F4BrLunMTA5a
-mnkPIAou1Z5jJh5VkpTYghdae9C8x49OhgQ=
------END CERTIFICATE-----
-EndOfCert
-
-dpkg-reconfigure ca-certificates
-echo "--------------------------------------------------------------------------------"
-ls -al /etc/ssl/certs
-
-exit 0
-
+# USERTrust RSA Certification Authority
 # http://crt.usertrust.com/USERTrustRSAAddTrustCA.crt
 cat > $CCBB_CERT_ROOT_DIR/USERTrustRSAAddTrustCA.crt <<EndOfCert
 -----BEGIN CERTIFICATE-----
@@ -80,6 +42,12 @@ Jtl7GQVoP7o81DgGotPmjw7jtHFtQELFhLRAlSv0ZaBIefYdgWOWnU914Ph85I6p
 0fKtirOMxyHNwu8=
 -----END CERTIFICATE-----
 EndOfCert
+
+dpkg-reconfigure ca-certificates
+echo "--------------------------------------------------------------------------------"
+ls -al /etc/ssl/certs
+
+exit 0
 
 # http://crt.usertrust.com/InCommonRSAServerCA_2.crt
 cat > $CCBB_CERT_ROOT_DIR/InCommonRSAServerCA_2.crt <<EndOfCert
